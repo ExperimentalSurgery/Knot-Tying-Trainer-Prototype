c703d5778e9fd4fa8e7138e4870db7c4555f1ce9
Mark Peter <mark.m.peter@gmail.com>
2023-05-22 12:26:54 +0200

    setup TTS tools

--- Assets/Scripts/TrainingSteps/GestureBaseStep.cs
+++ Assets/Scripts/TrainingSteps/GestureBaseStep.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using NMY.VTT.Core;
-using NMY.VTT.VirtualAssistant;
 using UnityEngine;
 using UnityEngine.Localization;
 
@@ -10,7 +9,6 @@ namespace DFKI.NMY
     public class GestureBaseStep : VTTBaseListStep
     {
         [Header("Gesture Step")] 
-        [SerializeField] private AudioCollection textToSpeechData;
         [SerializeField] private LocalizedString stepTitle;
         [SerializeField] private LocalizedString stepDescription;
 
@@ -19,52 +17,53 @@ namespace DFKI.NMY
             base.ActivateEnter();
             UserInterfaceManager.instance.UpdateStepInfos(stepTitle,stepDescription);
 
+            /*
             AudioClip clip;
-            textToSpeechData.TryGetAudio("DE", out clip);
             if (clip) {
                 SFXManager.instance.PlayAudio(clip);
             }
+            */
         }
 
         protected override void ActivateImmediatelyEnter()
         {
             base.ActivateImmediatelyEnter();
             UserInterfaceManager.instance.UpdateStepInfos(stepTitle,stepDescription);
-            AudioClip clip;
-            textToSpeechData.TryGetAudio("DE", out clip);
+            //AudioClip clip;
+            /*textToSpeechData.TryGetAudio("DE", out clip);
             if (clip) {
                 SFXManager.instance.PlayAudio(clip);
-            }
-        }
+            }*/
+}
 
-        protected override void DeactivateEnter()
-        {
-            base.DeactivateEnter();
-            SFXManager.instance.StopAudio();
-        }
+protected override void DeactivateEnter()
+{
+base.DeactivateEnter();
+SFXManager.instance.StopAudio();
+}
 
-        protected override void DeactivateImmediatelyEnter()
-        {
-            base.DeactivateImmediatelyEnter();
-            SFXManager.instance.StopAudio();
-        }
+protected override void DeactivateImmediatelyEnter()
+{
+base.DeactivateImmediatelyEnter();
+SFXManager.instance.StopAudio();
+}
 
-        [SerializeField] private float nextStepDelay = 2.0f;
-        public virtual IEnumerator TriggerDelayedCompletion()
-        {
-            yield return new WaitForSeconds(nextStepDelay);
-            RaiseStepCompletedEvent();
-        }
+[SerializeField] private float nextStepDelay = 2.0f;
+public virtual IEnumerator TriggerDelayedCompletion()
+{
+yield return new WaitForSeconds(nextStepDelay);
+RaiseStepCompletedEvent();
+}
 
-        protected override void OnStepComplete()
-        {
-            
-        }
+protected override void OnStepComplete()
+{
 
-        protected override void OnStepReset() {
-        }
-        protected override void OnPause() => GestureSequencePlayer.instance.Pause();
-        protected override void OnResume() => GestureSequencePlayer.instance.Resume();
-        
-    }
+}
+
+protected override void OnStepReset() {
+}
+protected override void OnPause() => GestureSequencePlayer.instance.Pause();
+protected override void OnResume() => GestureSequencePlayer.instance.Resume();
+
+}
 }
