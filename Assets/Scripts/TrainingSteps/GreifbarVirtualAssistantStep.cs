adf9af096cdf7f6a8de65f69213ad1f5767d01a6
Mark Peter <mark.m.peter@gmail.com>
2023-07-26 16:06:52 +0200

    Experthands prepared to allow finger highlights

--- Assets/Scripts/TrainingSteps/GreifbarVirtualAssistantStep.cs
+++ Assets/Scripts/TrainingSteps/GreifbarVirtualAssistantStep.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using NMY.VirtualRealityTraining.Steps.VirtualAssistant;
 using UnityEngine;
 using UnityEngine.Localization;
@@ -9,6 +12,11 @@ namespace DFKI.NMY
          [Header("Localized Step Data")] 
          [SerializeField] private LocalizedString stepTitle;
          [SerializeField] private LocalizedString stepDescription;
+         
+         
+         [Header("Hand Highlight Config")]
+         [SerializeField] private List<FingerHighlightContainer> highlights = new List<FingerHighlightContainer>();
+
     
          public LocalizedString StepTitle{
             get => stepTitle;
@@ -19,5 +27,33 @@ namespace DFKI.NMY
             get => stepDescription;
             set => stepDescription = value;
         }
+
+
+        protected override async UniTask ClientStepActionAsync(CancellationToken ct)
+        {
+            base.ClientStepActionAsync(ct);
+            // Hand Highlighting
+            foreach (FingerHighlightContainer highlightConfig in highlights) {
+                UserInterfaceManager.instance.FingerHighlight(highlightConfig);
+            }
+
+        }
+        
+        // PRE STEP
+        protected override async UniTask PreStepActionAsync(CancellationToken ct)
+        {
+            await base.PreStepActionAsync(ct);
+            UserInterfaceManager.instance.ResetFingerHighlights();
+        }
+
+        // POST STEP
+        protected override async UniTask PostStepActionAsync(CancellationToken ct)
+        {
+            await base.PostStepActionAsync(ct);
+            UserInterfaceManager.instance.ResetFingerHighlights();
+        }
+
+        
+
     }
 }
