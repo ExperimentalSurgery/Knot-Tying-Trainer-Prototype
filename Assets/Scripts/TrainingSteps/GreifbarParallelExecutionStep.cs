d9394316a8051189cb00fc0a0eb966fbb3bd7ef1
Mark Peter <mark.m.peter@gmail.com>
2023-07-27 15:36:36 +0200

    make google tts package local

---
+++ Assets/Scripts/TrainingSteps/GreifbarParallelExecutionStep.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using NMY.VirtualRealityTraining.Steps;
+using UnityEngine;
+
+namespace DFKI.NMY
+{
+    // Override of ParallelExecutionStep for potential extensions in future releases
+    public class GreifbarParallelExecutionStep : ParallelExecutionStep {
+        
+    }
+}
