a858ac082a450bd1100957869bf4cf3253fe467d
Alexander Radacki <alex@radacki.de>
2023-08-01 16:45:09 +0200

    UPDATE: knotenbank Step for tension + next step trigger script

--- Assets/Scripts/TrainingSteps/KnotenbankStep.cs
+++ Assets/Scripts/TrainingSteps/KnotenbankStep.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
@@ -8,15 +9,19 @@ namespace DFKI.NMY
     public enum KnotenbankDataType {Contact,Tension} 
     public class KnotenbankStep : GreifbarBaseStep {
         
+        public enum ContactState{closeContact, openContact}
+
         [Header("KnotenbankStep")]
         [SerializeField] private KnotenbankDataType checkValue;
-        [Range(0,1)]
-        [SerializeField] private int targetValue = 1;
+        [Range(0,350)]
+        [SerializeField] private int targetTensionValue = 350;
+        [SerializeField] private ContactState targetContactState;
         [SerializeField] private float minHoldDuration = 2f;
 
         // runtime vars
         private int tmpInt;
-        private int contactVal;
+        private int tmpInt2;
+        private ContactState contactVal;
         private int tensionVal;
         private float remainingDuration;
         private SerialController knotBankSerialController;
@@ -43,8 +48,14 @@ namespace DFKI.NMY
         private void OnMessageArrived(object sender, MessageEventArgs e) {
             string[] data = e.message.Split(';');
             if (int.TryParse(data[0], out tmpInt)) {
-                contactVal = tmpInt;
+                if(tmpInt == 0 || tmpInt == 1)
+                    contactVal = (ContactState)tmpInt;
             }
+            if (int.TryParse(data[1], out tmpInt2)) {
+                tensionVal = tmpInt2 * -1;
+            }
+
+            Debug.Log("data: " + tmpInt + " " + tmpInt2);
         }
         protected override async UniTask PostStepActionAsync(CancellationToken ct) {
             await base.PostStepActionAsync(ct);
@@ -62,10 +73,10 @@ namespace DFKI.NMY
             if (base.stepState.Equals(StepState.StepStarted)) {
                 switch (checkValue) {
                     case KnotenbankDataType.Contact:
-                        remainingDuration = contactVal == targetValue ? (remainingDuration - Time.deltaTime) : minHoldDuration;
+                        remainingDuration = contactVal == targetContactState ? (remainingDuration - Time.deltaTime) : minHoldDuration;
                         break;
                     case KnotenbankDataType.Tension:
-                        remainingDuration = tensionVal == targetValue ? (remainingDuration - Time.deltaTime) : minHoldDuration;
+                        remainingDuration = tensionVal >= targetTensionValue ? (remainingDuration - Time.deltaTime) : minHoldDuration;
                         break;
                 }
 
